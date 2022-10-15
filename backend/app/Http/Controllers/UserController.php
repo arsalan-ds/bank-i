361572850530b226e41b2fdcca437a89213c7e0f
arsalanDevelopers <arslan.mustafa@developers.studio>
2022-10-15 21:42:43 +0500

    update

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -25,18 +25,6 @@ class UserController extends Controller
             'lastName' => $user->last_name,
             'email' => $user->email,
         ];
-        $url = 'https://jsonplaceholder.typicode.com/todos/1';
-        // $url = config('constants.CREATE_ADDRESS_URL');
-        $createAddress = HttpRequest::sendRequest($url, $userData, false);
-        if($createAddress){
-        //    $address = $createAddress->address;
-           $address = '123456789poiuytresdfghjklmnbvcx';
-           $updateUser = $this->user->updateAddress($address, $user->id);
-           if($updateUser){
-               return $this->successResponse(new UserResource($updateUser), ResponseMessage::OK);
-           }
-           return $this->failureResponse(ResponseMessage::MESSAGE_500);
-        }
-        return $this->failureResponse(ResponseMessage::MESSAGE_500);
+        return $this->successResponse(new UserResource($user), ResponseMessage::OK);
     }
 }
